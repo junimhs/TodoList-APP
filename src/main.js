c54aee89ad51f56e26e175ea9efb5d45188390bd
Luis Henrique <junimhs10@gmail.com>
2021-02-23 18:44:32 -0300

    add package axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './plugins/vee-validate';
+import './plugins/axios';
 
 import './assets/css/tailwind.css';
 
