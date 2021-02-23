c269d7e53b1125ad634e2542618ec9339f57688f
Luis Henrique <junimhs10@gmail.com>
2021-02-23 18:43:58 -0300

    add package vee-validate

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './assets/css/tailwind.css';
 import router from './router';
 import store from './store';
+import './plugins/vee-validate';
+
+import './assets/css/tailwind.css';
+
 
 Vue.config.productionTip = false;
 
