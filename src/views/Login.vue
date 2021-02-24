08dec00c0d9819d5d36f486f16b7283edb362b8b
Luis Henrique <junimhs10@gmail.com>
2021-02-23 22:36:52 -0300

    add validate at we inputs

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,49 +2,82 @@
     <div style="height: 300px;">
         <LoginMenu />
 
-        <div class="grid gap-2">
-            <input
-                v-model="email"
-                type="text"
-                placeholder="Digite seu e-mail"
-                class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
-            >
-
-            <input
-                v-model="password"
-                type="password"
-                placeholder="Digite sua senha"
-                class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
-            >
-
-            <button
-                type="submit"
-                class="flex items-center justify-center bg-blue-800 text-blue-200 font-medium text-sm focus:outline-none rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
-                @click.stop.prevent="login"
-            >
-                ENTRAR
-            </button>
-
-            <div class="my-4 text-center">
-                <a
-                    class="text-sm font-light"
+        <ValidationObserver
+            tag="form"
+            ref="loginForm"
+            @submit.stop.prevent="login"
+        >
+            <div class="grid gap-2">
+                <ValidationProvider
+                    v-slot="{ errors }"
+                    name="E-mail"
+                    rules="required|email"
                 >
-                    Esqueci minha senha
-                </a>
+                    <input
+                        v-model="email"
+                        type="text"
+                        placeholder="Digite seu e-mail"
+                        class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
+                    >
+                    <div
+                        v-if="!!errors[0]"
+                        class="text-red-500 text-sm mb-2 mt-1"
+                    >
+                        {{ errors[0] }}
+                    </div>
+                </ValidationProvider>
+
+                <ValidationProvider
+                    v-slot="{ errors }"
+                    name="Senha"
+                    rules="required"
+                >
+                    <input
+                        v-model="password"
+                        type="password"
+                        placeholder="Digite sua senha"
+                        class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
+                    >
+                    <div
+                        v-if="!!errors[0]"
+                        class="text-red-500 text-sm mb-2 mt-1"
+                    >
+                        {{ errors[0] }}
+                    </div>
+                </ValidationProvider>
+
+                <button
+                    type="submit"
+                    class="flex items-center justify-center bg-blue-800 text-blue-200 font-medium text-sm focus:outline-none rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
+                >
+                    ENTRAR
+                </button>
+
+                <div class="my-4 text-center">
+                    <RouterLink
+                        :to="{name: 'forgot-password'}"
+                        class="text-sm font-light"
+                    >
+                        Esqueci minha senha
+                    </RouterLink>
+                </div>
             </div>
-        </div>
+        </ValidationObserver>
     </div>
 </template>
 
 <script>
     import LoginMenu from '../components/Auth/LoginMenu';
     import Cookie from 'js-cookie';
+    import { ValidationObserver, ValidationProvider } from 'vee-validate';
 
     export default {
         name: 'Login',
 
         components: {
             LoginMenu,
+            ValidationObserver,
+            ValidationProvider,
         },
 
         data() {
@@ -54,7 +87,11 @@
             };
         },
         methods: {
-            login() {
+            async login() {
+                const validate = await this.$refs.loginForm.validate();
+
+                if(!validate) return;
+
                 const payload = {
                     'email': this.email,
                     'password': this.password,
